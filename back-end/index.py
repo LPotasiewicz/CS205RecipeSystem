8325d0fb2b740ea3d2c7c3269ece783239a6a1ea
Devon Havers <devon66h@gmail.com>
2018-10-31 17:06:50 -0400

    Added basic MySQL connection, NOTE: not proper host, user or pass

--- back-end/index.py
+++ back-end/index.py
@@ -1,6 +1,12 @@
 import flask from Flask
+import mysql.connector
 
 app = Flask(__name__)
+database = mysql.connector.connect(
+    host="host",
+    user="username",
+    password="password"
+)
 
 @app.route("/")
 def index:
