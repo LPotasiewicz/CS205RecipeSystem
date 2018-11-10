263c6cb01208466969df3df08d68b583105ef9fc
lukepotasiewicz <33333105+lukepotasiewicz@users.noreply.github.com>
2018-11-10 14:08:37 -0500

    added recipe header and put it in recipe list

---
+++ front-end/src/recipe-header.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+class RecipeHeader extends Component {
+    static propTypes = {
+        title: PropTypes.string
+    };
+
+    render() {
+        return (
+            <div className="recipe-header">
+                <h3>{this.props.title}</h3>
+                <h4>{"Author"}</h4>
+                <p>{"Description of recipe 2018 (not clickbait) (cops called)"}</p>
+                <img src={"//lh4.googleusercontent.com/-ca8EQoEIcBo/AAAAAAAAAAI/AAAAAAAAECw/VugnJIvzv6U/s50-c-k-no/photo.jpg"}/>
+            </div>
+        );
+    }
+}
+export {RecipeHeader};
--- front-end/src/recipe-list.jsx
+++ front-end/src/recipe-list.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+import {RecipeHeader} from "./recipe-header";
 
 class RecipeList extends Component {
     static propTypes = {
@@ -8,9 +9,13 @@ class RecipeList extends Component {
     };
 
     render() {
+        const recipes = ["recipe 1", "recipe 2", "recipe 3", "recipe 4", "recipe 5"];
         return (
             <div className="recipe-list">
                 <h2>{"Recipe List"}</h2>
+                {recipes.map((r)=>
+                    <RecipeHeader title={r}/>
+                )}
             </div>
         );
     }
