3f2852c434cda67d69025b766aefa098be75ec8c
Devon Havers <devon66h@gmail.com>
2018-11-11 16:42:40 -0500

    Added GET and POST API

--- back-end/app.py
+++ back-end/app.py
@@ -1,34 +1,21 @@
-from flask import Flask, jsonify, request
-from flask.ext.pymongo import PyMongo
+from flask import Flask, jsonify
+from pymongo import PyMongo
 
 app = Flask(__name__)
+client = MongoClient("") #PUT URI HERE
 
-app.config['MONGO_DBNAME'] = ''
-app.config['MONGO_URI'] = ''
+@app.route("/")
+def index():
+    return "Hello World!"
 
-mongo = PyMongo(app)
-
-@app.route('/users/<user>', methods=['GET'])
-def get_user():
-    user = mongo.db.user
-
-    query = user.find_one({'name' : name})
-    output = {'name' : query['name'],} #add all other user attributes
-
-    return jsonify({output})
-
-@app.route('/users/<user>', methods=['POST'])
-def add_user():
-    user = mongo.db.user
-
-    name = request.json['name'] #add other user's attributes
-
-    user_id = user.insert({'name' : name})
-    new_user = user.find_one({'id' : user_id})
-
-    output = {'name' : new_user['name']} # add user's attributes
-
-    return jsonify({output})
+# GET and POST API
+class Response(Resource):
+    def get(self, user_id):
+        return USERS[user_id]
+    def post(self, user):
+        args = parser.parse_args()
+        USERS[user_id] = {'user_id': args['user_id']}
+        return USER[user_id], 201
 
 if __name__ == '__main__':
     app.run(debug=True)
