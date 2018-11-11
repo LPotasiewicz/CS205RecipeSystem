a63e61e53c1e6ff97909f1b96fe85a29beb99e5c
Devon Havers <devon66h@gmail.com>
2018-11-10 19:28:51 -0500

    Added GET and POST methods for JSON objects

--- back-end/app.py
+++ back-end/app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify, request
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "ENTER URI INFO HERE"
+
+app.config['MONGO_DBNAME'] = ''
+app.config['MONGO_URI'] = ''
+
 mongo = PyMongo(app)
 
-@app.route("/")
-def home():
-    return render_template("../front-end/public/index.html")
+@app.route('/users/<user>', methods=['GET'])
+def get_user():
+    user = mongo.db.user
 
-if __name__ == "__main__":
+    query = user.find_one({'name' : name})
+    output = {'name' : query['name'],} #add all other user attributes
+
+    return jsonify({output})
+
+@app.route('/users/<user>', methods=['POST'])
+def add_user():
+    user = mongo.db.user
+
+    name = request.json['name'] #add other user's attributes
+
+    user_id = user.insert({'name' : name})
+    new_user = user.find_one({'id' : user_id})
+
+    output = {'name' : new_user['name']} # add user's attributes
+
+    return jsonify({output})
+
+if __name__ == '__main__':
     app.run(debug=True)
