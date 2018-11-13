5fd80312f99b957e655f412fbc105da5d8a6404e
Devon Havers <devon66h@gmail.com>
2018-11-12 19:02:16 -0500

    Added GET and POST methods (check README for GET and POST calling conventions)

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
-from pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
 # MongoDB connection
-app.config['MONGO_DBNAME'] = 'Test'
-app.config['MONGO_URI'] = ''
+#app.config["MONGO_URI"] = "mongodb://devon:1234@test-shard-00-00-i0x5r.mongodb.net:27017,test-shard-00-01-i0x5r.mongodb.net:27017,test-shard-00-02-i0x5r.mongodb.net:27017/test?ssl=true&replicaSet=Test-shard-0&authSource=admin&retryWrites=true"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -18,21 +20,20 @@ def get_users():
     for user in users.find():
         output.append({"name" : user["name"],
                        "email" : user["email"], "password" : user["password"]})
-    return jsonify({output})
+    return jsonify(output)
 
 @app.route("/api/recipes", methods=["GET"])
 def get_recipes():
     recipes = mongo.db.recipes
     output = []
-    for recipe in users.find():
+    for recipe in recipes.find():
         output.append({"title" : recipe["title"],
                        "author" : recipe["author"],
-                       "prep_time" : recipe["prep_time"],
                        "cook_time" : recipe["cook_time"],
                        "img_url" : recipe["img_url"],
                        "ingredients" : recipe["ingredients"],
                        "steps" : recipe["steps"]})
-    return jsonify({output})
+    return jsonify(output)
 
 #POST
 @app.route("/api/users", methods=["POST"])
@@ -43,24 +44,21 @@ def create_user():
     password = request.json["password"]
     new_user = user.insert({"name" : name, "email" : email,
                             "password" : password})
-    output = {"name" : new_user["name"], "email" : new_user["email"],
-              "password" : new_user["password"]]}
-    return jsonify({output})
+    return "Success"
 
 @app.route("/api/recipes", methods=["POST"])
 def create_recipe():
-    recipe = mongo.db.users
+    recipe = mongo.db.recipes
     title = request.json["title"]
     author = request.json["author"]
-    prep_time = request.json["prep_time"]
     cook_time = request.json["cook_time"]
     img_url = request.json["img_url"]
     ingredients = request.json["ingredients"]
     steps = request.json["steps"]
-    new_user = user.insert({"title" : title, "author" : author,
-                            "prep_time" : prep_time, "cook_time" : cook_time,
-                            "img_url", img_url, "ingredients" : ingredients,
-                            "steps", steps})
+    new_user = recipe.insert({"title" : title, "author" : author, "cook_time" : cook_time,
+                            "img_url": img_url, "ingredients" : ingredients,
+                            "steps": steps})
+    return "Success"
 
 if __name__ == '__main__':
     app.run(debug=True)
