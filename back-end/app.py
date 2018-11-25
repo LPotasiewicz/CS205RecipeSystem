8702b1aea97c663bcc54b1988e831c7202191450
Devon Havers <devon66h@gmail.com>
2018-11-25 14:57:19 -0500

    Still working on adding ability to search for a specific user and recipe, almost done

--- back-end/app.py
+++ back-end/app.py
@@ -3,18 +3,17 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-# MongoDB connection
-app.config['MONGO_URI'] = 'mongodb://devon:1234@test-shard-00-00-i0x5r.mongodb.net:27017,test-shard-00-01-i0x5r.mongodb.net:27017,test-shard-00-02-i0x5r.mongodb.net:27017/test?ssl=true&replicaSet=Test-shard-0&authSource=admin&retryWrites=true'
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/mydb"
+app.config["MONGO_URI"] = "mongodb://devon:1234@test-shard-00-00-i0x5r.mongodb.net:27017,test-shard-00-01-i0x5r.mongodb.net:27017,test-shard-00-02-i0x5r.mongodb.net:27017/test?ssl=true&replicaSet=Test-shard-0&authSource=admin&retryWrites=true"
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
 mongo = PyMongo(app)
+CORS(app)
 
 @app.route("/")
 def index():
     return "Hello World!"
 
-#GET
+#GET all users
 @app.route("/users", methods=["GET"])
 def get_users():
     users = mongo.db.users
@@ -24,6 +23,19 @@ def get_users():
                        "email" : user["email"], "password" : user["password"]})
     return jsonify(output)
 
+#GET one user by their name
+@app.route("/users/", methods=["GET"])
+def get_user(name):
+    users = mongo.db.users
+    user = users.find_one({"name" : name})
+    if user:
+        output = {"name" : user["name"], "email" : user["email"],
+                  "password" : user["password"]}
+    else:
+        output = "User not found"
+    return jsonify(output)
+
+#GET all recipes
 @app.route("/recipes", methods=["GET"])
 def get_recipes():
     recipes = mongo.db.recipes
@@ -37,7 +49,23 @@ def get_recipes():
                        "steps" : recipe["steps"]})
     return jsonify(output)
 
-#POST
+#GET one recipe by its title
+@app.route("/recipes/", methods=["GET"])
+def get_recipe(title):
+    recipes = mongo.db.recipes
+    recipe = recipes.find_one({"title" : title})
+    if recipe:
+        output = {"title" : recipe["title"],
+                  "author" : recipe["author"],
+                  "cook_time" : recipe["cook_time"],
+                  "img_url" : recipe["img_url"],
+                  "ingredients" : recipe["ingredients"],
+                  "steps" : recipe["steps"]}
+    else:
+        output = "Recipe not found"
+    return jsonify(output)
+
+#POST user
 @app.route("/users", methods=["POST"])
 def create_user():
     user = mongo.db.users
@@ -48,6 +76,7 @@ def create_user():
                             "password" : password})
     return "Success"
 
+#POST recipe
 @app.route("/recipes", methods=["POST"])
 def create_recipe():
     recipe = mongo.db.recipes
